cbfa17f433b291169314cbf665b7c1ed66dcd5b3
Nico <klaassen.nico@gmail.com>
2018-05-06 14:32:54 +0200

    Added circles and ellipses
    Also improved the code by adding some comments.

--- src/visualizations/opengl-demo-tree.ts
+++ src/visualizations/opengl-demo-tree.ts
@@ -20,6 +20,8 @@ export class OpenglDemoTree implements Visualizer {
         }
 
         // Some big green quad examples
+        // Note that the misalignment of the top-left rectangle is intentional to highlight it is being drawn based on
+        // an (x, y) center coordinate. Where-as (x, y) is the bottom-left coordinate for the other quads.
         gl.fillAAQuad(100, 100, 100, 100, [0, 1, 0, 1]);
         gl.drawAAQuad(100, -200, 100, 100, [0, 1, 0, 1]);
         gl.fillLinedAAQuad(-200, -200, 100, 100, [0, 1, 0, 1], [0, 0, 0, 1]);
@@ -30,7 +32,9 @@ export class OpenglDemoTree implements Visualizer {
         let startY = 300;
         let offsetX = 4;
         let quadWidth = 5;
-        let quadHeight = 100;
+        let quadHeight = 70;
+        let radiusX = quadWidth / 2;
+        let radiusY = quadHeight / 2;
         let rotationOffset = 1;
         let outlineColor = [0, 0, 0, 1];
         for (let i = 0; i <= 360; i++) {
@@ -43,18 +47,21 @@ export class OpenglDemoTree implements Visualizer {
             const x = startX + offsetX * i;
             const rotationDegrees = rotationOffset * i;
             gl.fillLinedRotatedQuad(x, startY, quadWidth, quadHeight, rotationDegrees, randomColor, outlineColor);
-            gl.fillLinedRotatedQuad(x, -startY, quadWidth, quadHeight, -rotationDegrees, randomColor, outlineColor);
+            gl.fillLinedEllipsoid(x, -startY, radiusX, radiusY, rotationDegrees, randomColor, outlineColor); // Optional, precision: number
         }
 
         // Rotation sparse example
         startX = -720;
-        startY = 150;
+        startY = 225;
         offsetX = 16;
         quadWidth = 10;
         quadHeight = 10;
+        radiusX = quadWidth / 2;
+        radiusY = quadHeight; // Not divided by two to highlight rotation with the ellipses
         rotationOffset = 4;
         outlineColor = [0, 0, 0, 1];
         for (let i = 0; i <= 90; i++) {
+            // Generate random color
             const randomColor = [
                 Math.random(),
                 Math.random(),
@@ -64,9 +71,28 @@ export class OpenglDemoTree implements Visualizer {
             const x = startX + offsetX * i;
             const rotationDegrees = rotationOffset * i;
             gl.fillLinedRotatedQuad(x, startY, quadWidth, quadHeight, rotationDegrees, randomColor, outlineColor);
-            gl.fillLinedRotatedQuad(x, -startY, quadWidth, quadHeight, -rotationDegrees, randomColor, outlineColor);
+            gl.fillLinedEllipsoid(x, -startY, radiusX, radiusY, rotationDegrees, randomColor, outlineColor); // optional, precision: number
         }
 
+        // Sine wave example
+        startX = -720;
+        startY = 30;
+        let amplitude = 10;
+        let radius = 2;
+        let fillColor = [1, 0, 0, 1];
+        outlineColor = [0, 0, 0, 1];
+
+        for (let i = 0; i <= 360; i++) {
+            const x = i / Math.PI ;
+            const y = amplitude * Math.sin(x) + startY; // Standard mathematical sine curve form; a + b*sin(c (x-d) )
+            if (i % 2 === 0) {
+                gl.drawCircle(startX + i * 4, y, radius, outlineColor); // optional, precision: number
+                gl.drawCircle(startX + i * 4, -y, radius, outlineColor); // optional, precision: number
+            } else {
+                gl.fillCircle(startX + i * 4, y, radius, fillColor); // optional, precision: number
+                gl.fillCircle(startX + i * 4, -y, radius, fillColor); // optional, precision: number
+            }
+        }
 
         /** @end-author Nico Klaassen */
     }
