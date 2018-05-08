f0807caaa0e18c389880849a2e1f2715f4340069
Nico <klaassen.nico@gmail.com>
2018-05-08 03:30:39 +0200

    Implemented settings for OpenglDemoTree

--- src/visualizations/opengl-demo-tree.ts
+++ src/visualizations/opengl-demo-tree.ts
@@ -1,11 +1,25 @@
 import {Visualizer} from '../interfaces/visualizer';
 import {Node} from '../models/node';
 import {OpenGL} from '../opengl/opengl';
+import {Form} from "../form/form";
+import {FormFactory} from "../form/form-factory";
 
-
+/** @author Nico Klaassen */
 export class OpenglDemoTree implements Visualizer {
+    private sinewaves: boolean;
+    private gl: OpenGL;
+    private tree: Node;
+    private mainColor: number[];
+
+    constructor() {
+        // Initialize settings
+        this.mainColor = [0, 1, 0, 1];
+        this.sinewaves = true;
+    }
+
     public draw(tree: Node, gl: OpenGL) {
-        /** @author Nico Klaassen */
+        this.gl = gl; // So we have access to this in the entire class.
+        this.tree = tree;
 
         // Draw axis - range and domain: [-100, 100]
         gl.fillAAQuad(-200, -1, 400, 2, [1, 0, 0, 1]); // X axis
@@ -22,10 +36,10 @@ export class OpenglDemoTree implements Visualizer {
         // Some big green quad examples
         // Note that the misalignment of the top-left rectangle is intentional to highlight it is being drawn based on
         // an (x, y) center coordinate. Where-as (x, y) is the bottom-left coordinate for the other quads.
-        gl.fillAAQuad(100, 100, 100, 100, [0, 1, 0, 1]);
-        gl.drawAAQuad(100, -200, 100, 100, [0, 1, 0, 1]);
-        gl.fillLinedAAQuad(-200, -200, 100, 100, [0, 1, 0, 1], [0, 0, 0, 1]);
-        gl.drawRotatedQuad(-200, 100, 100, 100, 45, [0, 1, 0, 1]);
+        gl.fillAAQuad(100, 100, 100, 100, this.mainColor);
+        gl.drawAAQuad(100, -200, 100, 100, this.mainColor);
+        gl.fillLinedAAQuad(-200, -200, 100, 100, this.mainColor, [0, 0, 0, 1]);
+        gl.drawRotatedQuad(-200, 100, 100, 100, 45, this.mainColor);
 
         // Rotation dense example
         let startX = -720;
@@ -74,30 +88,59 @@ export class OpenglDemoTree implements Visualizer {
             gl.fillLinedEllipsoid(x, -startY, radiusX, radiusY, rotationDegrees, randomColor, outlineColor); // optional, precision: number
         }
 
-        // Sine wave example
-        startX = -720;
-        startY = 30;
-        let amplitude = 10;
-        let radius = 2;
-        let fillColor = [1, 0, 0, 1];
-        outlineColor = [0, 0, 0, 1];
+        if (this.sinewaves) {
+            // Sine wave example
+            startX = -720;
+            startY = 30;
+            let amplitude = 10;
+            let radius = 2;
+            let fillColor = [1, 0, 0, 1];
+            outlineColor = [0, 0, 0, 1];
 
-        for (let i = 0; i <= 360; i++) {
-            const x = i / Math.PI ;
-            const y = amplitude * Math.sin(x) + startY; // Standard mathematical sine curve form; a + b*sin(c (x-d) )
-            if (i % 2 === 0) {
-                gl.drawCircle(startX + i * 4, y, radius, outlineColor); // optional, precision: number
-                gl.drawCircle(startX + i * 4, -y, radius, outlineColor); // optional, precision: number
-            } else {
-                gl.fillCircle(startX + i * 4, y, radius, fillColor); // optional, precision: number
-                gl.fillCircle(startX + i * 4, -y, radius, fillColor); // optional, precision: number
+            for (let i = 0; i <= 360; i++) {
+                const x = i / Math.PI;
+                const y = amplitude * Math.sin(x) + startY; // Standard mathematical sine curve form; a + b*sin(c (x-d) )
+                if (i % 2 === 0) {
+                    gl.drawCircle(startX + i * 4, y, radius, outlineColor); // optional, precision: number
+                    gl.drawCircle(startX + i * 4, -y, radius, outlineColor); // optional, precision: number
+                } else {
+                    gl.fillCircle(startX + i * 4, y, radius, fillColor); // optional, precision: number
+                    gl.fillCircle(startX + i * 4, -y, radius, fillColor); // optional, precision: number
+                }
             }
         }
-
-        /** @end-author Nico Klaassen */
     }
+
+    public getForm(formFactory: FormFactory) {
+        return formFactory.createFormBuilder()
+            .addNumberField('r', 0, { label: 'Red' })
+            .addNumberField('g', 255, { label: 'Green' })
+            .addNumberField('b', 0, { label: 'Blue' })
+            .addNumberField('a', 255, { label: 'Alpha'})
+            .addToggleField('sinewaves', true, { label: 'Sine waves' })
+            .getForm();
+    }
+
+    public applySettings(settings: object){
+        console.log(settings);
+
+        // Update the color of the 4 big center objects
+        this.mainColor = [
+            settings.r / 255,
+            settings.g / 255,
+            settings.b / 255,
+            settings.a / 255
+        ]
+        this.sinewaves = settings.sinewaves; // update whether we should draw the sine waves
+
+        this.gl.releaseBuffers();       // remove old data from buffers
+        this.draw(this.tree, this.gl);  // fill buffers with new data
+        this.gl.render();               // force a render
+    }
+
 
     public getName(): string {
         return 'OpenGL Demo Tree';
     }
 }
+/** @end-author Nico Klaassen */
