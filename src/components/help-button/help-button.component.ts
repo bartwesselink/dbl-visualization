cdd8810ffe41387c86276377ce081ae6fa2b8bfb
Masboes <mathijs.boezer@gmail.com>
2018-05-11 09:41:10 +0200

    The help tour can now interact with the elements on the page by clicking on specific elements. This allows the tour to open menus for example. It also supports elements that are added during the tour, like the canvas in the current tour.

--- src/components/help-button/help-button.component.ts
+++ src/components/help-button/help-button.component.ts
@@ -34,17 +34,25 @@ export class HelpButtonComponent implements OnInit{
                     element: 'app-upload-tool label'
                 },
                 {
-                    intro: "Here you will be able to see your dataset as a tree with nodes you can collapse and expand.",
-                    element: '.sidebar-content',
-                    position: "left"
-                },
-                {
                     intro: "This bar contains the visualization tabs, each tab represents a visualization of your dataset.",
                     element: '.mdl-layout__tab-bar-container'
                 },
                 {
                     intro: "To add a new visualization to the tabs, click here and choose one of the options.",
-                    element: 'app-visualization-picker > button'
+                    element: 'app-visualization-picker > button',
+                    click: true,
+                    clickTime: 'exit'
+                },
+                {
+                    intro: "For this tour we will use this one as an example.",
+                    element: 'app-visualization-picker div ul li',
+                    click: true,
+                    clickTime: 'exit'
+                },
+                {
+                    intro: "Here you will be able to see your dataset as a tree with nodes you can collapse and expand.",
+                    element: '.sidebar-content',
+                    position: "left"
                 },
                 {
                     intro: "This is the visualization window, your dataset will be visualized here according to your selected visualization.",
@@ -52,11 +60,17 @@ export class HelpButtonComponent implements OnInit{
                 },
                 {
                     intro: "If you want to change something about the current visualization, click here.",
-                    element: '.mdl-layout__tab-panel.is-active app-settings-button button'
+                    element: '.mdl-layout__tab-panel.is-active app-visualization-settings-button button',
                 },
                 {
                     intro: "If you want to make a screenshot of your visualization window, click here.",
-                    element: '.mdl-layout__tab-panel.is-active app-screenshot-button button'
+                    element: '.mdl-layout__tab-panel.is-active app-screenshot-button button',
+                },
+                {
+                    intro: "Finally you can close a visualization by clicking here",
+                    element: '.mdl-layout__tab-app.is-active i',
+                    click: true,
+                    clickTime: 'exit',
                 }
             ],
             showStepNumbers: false, // Hide step numbers
@@ -65,6 +79,46 @@ export class HelpButtonComponent implements OnInit{
             overlayOpacity: 0.5,    // Set opacity of page overlay to 0.5
             nextLabel: "Next",      // Default has arrow, doesn't fit with material style
             prevLabel: "Back",      // Default has arrow, doesn't fit with material style
+        });
+
+        // This implements the click attribute in the steps so we can do things like opening menus in the tour
+        // The clickTime attribute determines when in a step the click event is triggered
+        // start means right when the tour gets to the element, exit right when it goes to the next
+        // By default clickTime is 'start'
+        let self = this;
+        this.tour.onchange(function(element) {
+            if(this._options.steps[this._currentStep].click && this._options.steps[this._currentStep].clickTime != 'exit' && element) {
+                element.click();
+            }
+
+            if(this._options.steps[this._currentStep - 1] &&
+                this._options.steps[this._currentStep - 1].click &&
+                this._options.steps[this._currentStep - 1].clickTime == 'exit') {
+                (document.querySelector(this._options.steps[this._currentStep - 1].element) as HTMLElement).click();
+            }
+
+            for (var i = this._currentStep; i < this._options.steps.length; i++) {
+                var currentItem = this._introItems[i];
+                var step = this._options.steps[i];
+
+                if (step.element && document.querySelector(step.element)) {
+                    currentItem.element = (document.querySelector(step.element) as HTMLElement);
+                    currentItem.position = step.position;
+                }
+
+                if(step.element == '.mdl-layout__tab-panel.is-active') {
+                    console.log(document.querySelector(step.element) as HTMLElement);
+                }
+            }
+        });
+
+        // Edge case for triggering a click on exiting the last step of the tour
+        this.tour.onbeforeexit(function() {
+            if(this._options.steps[this._currentStep - 1] &&
+                this._options.steps[this._currentStep - 1].click &&
+                this._options.steps[this._currentStep - 1].clickTime == 'exit') {
+                (document.querySelector(this._options.steps[this._currentStep - 1].element) as HTMLElement).click();
+            }
         })
     }
 
