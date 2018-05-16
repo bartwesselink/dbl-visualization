5dd2c7a12308fe0fdfb2f6540c5a474dafbfa925
Bart Wesselink <bartwesselink99@gmail.com>
2018-05-16 10:51:55 +0200

    Hotfix for resize events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,8 @@ export class AppComponent {
         this.settingsBus.settingsChanged.subscribe((settings: Settings) => {
             console.log(settings);
         });
+
+        window.addEventListener('resize', () => this.resizeActiveTab());
     }
 
     /** @author Jordy Verhoeven */
@@ -93,6 +95,10 @@ export class AppComponent {
     }
 
     private resizeActiveTab(): void {
+        if (!this.activeTab) {
+            return;
+        }
+
         setTimeout(() => {
             this.activeTab.window.setHeight();
         });
