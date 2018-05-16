c94f18cf948fa5a185d41a52c7bbaca697e6d6f5
Nico <klaassen.nico@gmail.com>
2018-05-17 00:43:52 +0200

    updated the offset implementation

--- src/visualizations/simple-tree-map.ts
+++ src/visualizations/simple-tree-map.ts
@@ -57,9 +57,9 @@ export class SimpleTreeMap implements Visualizer {
     }
 
     /** drawTree draw the tree-map recursively.
+     *
      * @param {Node} tree The root of the subtree upon which we recurse
      * @param {Bounds} bounds The bounding-box indicating where we should draw the current root
-     * @param {Orientation} orientation In which direction this node is oriented which indicates how children should be oriented
      * @param {boolean} internalNode Whether we are recursing on internal nodes, or on the root of the initial input tree
      * @param {number[]} color The color with which we should draw our current bounding-box based rectangle
      */
@@ -94,63 +94,56 @@ export class SimpleTreeMap implements Visualizer {
         }
     }
 
-    /** setBounds calculates the new and nested bounding-box (bounds) for a particular child-node in relation to the
-     * bounds of its parent.
+    /** setBounds calculates the new and nested bounding-box (bounds) for a particular child-node and stores it on the
+     * node itself.
+     *
+     * @param {Node} tree The (sub)tree for which to calculate bounds
      * @param {Bounds} parentBounds The bounding-box of the parent node
      * @param {number} doneSize How many descendants of the parent node are already accounted for by other siblings.
-     * @param {number} parentSize How many descendants the parent node has
-     * @param {number} childSize How many nodes there are in the subtree rooted at the node for which we calculate
-     * @param {Orientation} parentOrientation The direction in which the parent has been laid out
+     * @param {boolean} last Whether this is the last of the children
+     * @param {number} index The index of the child within the array containing its siblings (on the parent node)
      * @returns {Bounds} New bounding-box with the correct position and offset such that it is nested within parentBounds
      */
     private setBounds(tree: Node, parentBounds: Bounds, doneSize: number, last: boolean, index: number): Bounds {
         const parentWidth = Math.abs(parentBounds.right - parentBounds.left);
         const parentHeight = Math.abs(parentBounds.top - parentBounds.bottom);
-        const parentSize = tree.parent.subTreeSize;
+        const parentSize = tree.parent.subTreeSize - 1;
         const childSize = tree.subTreeSize;
 
         // Compute the new bounds which are nested within the bounds of the parent
         if (tree.parent.orientation === Orientation.HORIZONTAL) {
-            // const offsetCompensation = (index + 1) * (parentWidth - parentWidth * (tree.parent.children.length + 1) * this.offset / tree.parent.children.length);
-            const offsetCompensation = (parentWidth - this.offset * tree.parent.children.length) / tree.parent.children.length;
-            console.log("comp: " + offsetCompensation);
+            const freeSpace = parentWidth - (tree.parent.children.length + 1) * this.offset;
             return {
                 left: (index == 0) ?
                     parentBounds.left + this.offset :
-                    parentBounds.left + parentWidth * doneSize / parentSize + this.offset * (index + 1) - offsetCompensation * (index + 1),
+                    parentBounds.left + this.offset * (index + 1) + (freeSpace * doneSize / parentSize),
                 right: (last) ?
-                    parentBounds.left + parentWidth * doneSize / parentSize + parentWidth * childSize / parentSize + this.offset * (index + 1) - offsetCompensation * (index + 1) :
-                    parentBounds.left + parentWidth * doneSize / parentSize + this.offset * (index + 1) - offsetCompensation * (index + 1),
+                    parentBounds.left + this.offset * (index + 1) + freeSpace :
+                    parentBounds.left + this.offset * (index + 1) + (freeSpace * (doneSize + childSize) / parentSize),
                 bottom: parentBounds.bottom + this.offset,
                 top: parentBounds.top - this.offset
             };
         } else {
+            const freeSpace = parentHeight - (tree.parent.children.length + 1) * this.offset;
             return {
                 left: parentBounds.left + this.offset,
                 right: parentBounds.right - this.offset,
-                bottom: last ?
-                    parentBounds.top - parentHeight * doneSize / parentSize - parentHeight * childSize / parentSize + this.offset :
-                    parentBounds.top - parentHeight * doneSize / parentSize - parentHeight * childSize / parentSize + this.offset / 2,
-                top: parentBounds.top - parentHeight * doneSize / parentSize - this.offset / 2
+                bottom: (last) ?
+                    parentBounds.top - this.offset * (index + 1) - freeSpace :
+                    parentBounds.top - this.offset * (index + 1) - (freeSpace * (doneSize + childSize) / parentSize),
+                top: (index == 0) ?
+                    parentBounds.top - this.offset :
+                    parentBounds.top - this.offset * (index + 1) - (freeSpace * doneSize / parentSize)
             };
-        //     return {
-        //         left: parentBounds.left + parentWidth * doneSize / parentSize + this.offset / 2,
-        //         right: last ? parentBounds.left + parentWidth * doneSize / parentSize + parentWidth * childSize / parentSize - this.offset : parentBounds.left + parentWidth * doneSize / parentSize + parentWidth * childSize / parentSize - this.offset / 2,
-        //         bottom: parentBounds.bottom + this.offset,
-        //         top: parentBounds.top - this.offset
-        //     };
-        // } else {
-        //     return {
-        //         left: parentBounds.left + this.offset,
-        //         right: parentBounds.right - this.offset,
-        //         bottom: last ? parentBounds.top - parentHeight * doneSize / parentSize - parentHeight * childSize / parentSize + this.offset : parentBounds.top - parentHeight * doneSize / parentSize - parentHeight * childSize / parentSize + this.offset / 2,
-        //         top: parentBounds.top - parentHeight * doneSize / parentSize - this.offset / 2
-        //     };
         }
-
     };
 
-    private orientTreeNodes(tree: Node): void{
+    /**
+     * Function which augments the tree data structure and adds in an orientation.
+     *
+     * @param {Node} tree Tree for which to calculate the orientation of the nodes for
+     */
+    private orientTreeNodes(tree: Node): void {
         // Toggle the orientation for direct children of the current node
         if (tree.orientation === Orientation.HORIZONTAL) {
             var childOrientation = Orientation.VERTICAL;
@@ -165,6 +158,13 @@ export class SimpleTreeMap implements Visualizer {
         }
     }
 
+    /**
+     * Function which calculates the height of the given tree recursively
+     *
+     * @param {Node} tree Tree for which to calculate the height for
+     * @param {number} currentHeight Initially should be 0, variable to track current height.
+     * @returns {number} The height of the tree
+     */
     private calculateTreeHeight(tree: Node, currentHeight: number): number {
         let treeHeight = currentHeight;
         for (let i = 0; i < tree.children.length; i++) {
@@ -180,6 +180,14 @@ export class SimpleTreeMap implements Visualizer {
         return treeHeight;
     }
 
+    /**
+     * Recursive method to calculate the maximum width or height in terms of cell/segment count for a given tree.
+     *
+     * @param {Node} tree The tree for which to calculate the max segment width or height
+     * @param {number} horizontalSegments Initial call should be 0 , parameter to track width recursively
+     * @param {number} verticalSegments Initial call should be 0 , parameter to track height recursively
+     * @returns {number} The maximum width or height in terms of cell / segment count
+     */
     private calculateMaxSegments(tree: Node, horizontalSegments: number, verticalSegments: number): number {
         let maxSegments = Math.max(horizontalSegments, verticalSegments);
 
@@ -217,15 +225,17 @@ export class SimpleTreeMap implements Visualizer {
     public applySettings(settings: any) {
         this.drawOutlines = settings.outline;
         this.offset = settings.offset;
+
+        // We have to create space to allow for the absolute offset of each cell
+        // Using this currently enables the best implementation for offset in all
+        // my extensive attempts thus far.
         if (this.offset > 0) {
             const oldBounds = this.rootBounds;
             this.updateRootBounds();
-            const scaleFactor = (oldBounds.right / this.rootBounds.right);
-            console.log("scalefactor: " + scaleFactor);
-            this.gl.scale(scaleFactor);
-            this.offsetScale = (300 / this.rootBounds.right);
-            this.offset *= 1;
-            console.log("offset scale: " + this.offsetScale);
+            const scaleFactor = (oldBounds.right / this.rootBounds.right); // Calculate a scalingfactor to prevent the visualization from moving
+            this.gl.scale(scaleFactor); // Actually scale
+            this.offsetScale = (300 / this.rootBounds.right); // Factor indicating how far away we are from normal scaling
+            this.offset = Math.pow(this.offset, 1.4); // Increase scaling factor for the offset, this can be tweaked, though 1.4 works well for now
         }
 
         this.gl.releaseBuffers();       // remove old data from buffers
@@ -233,7 +243,11 @@ export class SimpleTreeMap implements Visualizer {
         this.gl.render();               // force a render
     }
 
+    /**
+     * Utility function to calculate the new size for the upscaled base-square of the visualization
+     */
     public updateRootBounds(): void {
+        // Caluclate how much offset we need at most based on width/height segment count.
         const maxSegments = this.calculateMaxSegments(this.tree, 0, 0);
 
         this.rootBounds = {
