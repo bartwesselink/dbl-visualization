dbe8087cbabd88b15278c39c51a6d8bdab5d1a5c
Mathijs B <mathijs.boezer@gmail.com>
2018-05-19 19:14:00 +0200

    Added rest of the page to the dark mode

--- src/utils/darkmode.ts
+++ src/utils/darkmode.ts
@@ -2,13 +2,13 @@ export class DarkMode {
     /** @author Mathijs Boezer */
 
     public set(enabled: boolean): void {
-        if(!document.querySelector(".mdl-layout__container")) {
+        if(!document.querySelector("body")) {
             return;
-        } // container does not (yet) exist
+        }
         if(enabled) {
-            document.querySelector(".mdl-layout__container").classList.add("dark-mode");
+            document.querySelector("body").classList.add("dark-mode");
         } else {
-            document.querySelector(".mdl-layout__container").classList.remove("dark-mode");
+            document.querySelector("body").classList.remove("dark-mode");
         }
     }
     /** @end-author Mathijs Boezer */
