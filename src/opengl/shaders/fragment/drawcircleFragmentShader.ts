797a06b61d41a5426366e911fe367628d8aaec35
Roan <roan.hofland@hotmail.com>
2018-05-31 23:45:09 +0200

    Lower line width

--- src/opengl/shaders/fragment/drawcircleFragmentShader.ts
+++ src/opengl/shaders/fragment/drawcircleFragmentShader.ts
@@ -9,7 +9,7 @@ export const fragmentSource = `
     
     void main() {
         lowp float val = pow(vpos.x - cx, 2.0) * 3.1604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938271604938272 + pow(vpos.y - cy, 2.0);
-        if(val <= radius * radius && val >= pow(radius - 0.005, 2.0)){
+        if(val <= radius * radius && val >= pow(radius - 0.004, 2.0)){
             gl_FragColor = vec4(color, 1.0);
         }else{
             discard;
