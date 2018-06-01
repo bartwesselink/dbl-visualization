4a59eb0cdaee30cc04315fbcae9c776f3ff76571
Roan <roan.hofland@hotmail.com>
2018-06-01 19:15:38 +0200

    This does not work yet

--- src/opengl/shaders/fragment/fillCircleSliceFragmentShader.ts
+++ src/opengl/shaders/fragment/fillCircleSliceFragmentShader.ts
@@ -1,5 +1,8 @@
 /** @author Roan Hofland */
 export const fragmentSource = `
+    #define PI 3.1415926535897932384626433832795
+    #define ratio 1.7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777778
+
     uniform lowp float radius;
     uniform lowp float cx;
     uniform lowp float cy;
@@ -10,11 +13,11 @@ export const fragmentSource = `
     varying lowp vec2 vpos;
     
     void main() {
-        lowp float dx = (vpos.x - cx) * 1.7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777778;
+        lowp float dx = (vpos.x - cx) * ratio;
         lowp float dy = vpos.y - cy;
         lowp float val = sqrt(pow(dx, 2.0) + pow(dy, 2.0));
         lowp float angle = atan(dy, dx);
-        if(val <= radius && angle >= start && angle <= end){
+        if(val <= radius && angle >= start - PI && angle <= end - PI){
             gl_FragColor = vec4(color, 1.0);
         }
     }
