d76e1a7c386e931e5636d82e2f002f8076d23d15
teards <teards@nooby.eu>
2018-06-01 12:15:05 +0200

    GPT: Settings, code cleanliness, height scaling

--- src/visualizations/generalized-pythagoras-tree.ts
+++ src/visualizations/generalized-pythagoras-tree.ts
@@ -1,6 +1,5 @@
 import {Visualizer} from '../interfaces/visualizer';
 import {Node} from '../models/node';
-import {OpenGL} from '../opengl/opengl';
 import {Form} from '../form/form';
 import {FormFactory} from '../form/form-factory';
 import {Draw} from '../interfaces/draw';
@@ -13,17 +12,10 @@ export class GeneralizedPythagorasTree implements Visualizer {
         const tree = input.tree;
         const draws: Draw[] = [];
         const palette: Palette = input.palette;
-        // palette.printtoconsole();
-        // palette.calcGradientColorMap(tree);
 
-        const primaryColor = palette.primary;
-        const secondaryColor = palette.secondary;
-
-
-        const weightHeight: number = 1; // !! Currently broken !! Scaling factor for the height of every rectangle that is not the root
-        const defaultColor: number[] = [1, 0, 1, 0.5]; // Just some test color, translucency to show that no rectangles overlap
-        const selectedColor: number[] = [1, 0, 1, 1];
-        let color: number[] = defaultColor;
+        const weightHeight: number = input.settings.heightScale / 10; // Scaling factor for the height of every rectangle that is not the root
+        const drawNodeSize: boolean = input.settings.nodeSize;
+        let color: number[]; // Have to initialise it here otherwise typescript complains
 
         const initialRectangleCenterX: number = 0;
         const initialRectangleCenterY: number = -250;
@@ -35,18 +27,7 @@ export class GeneralizedPythagorasTree implements Visualizer {
 
         // Define the base rectangle of the visualized tree, these values can later be determined in settings
         // Rectangle is an array of 5 numbers, in order: center x, center y, width (X), height (Y), angle
-        let rectangle = [initialRectangleCenterX, initialRectangleCenterY, initialRectangleWidth, initialRectangleHeight, initialRectangleAngle];
-
-        //return Color.HSBtoRGB(subTreeSize / 256.0F, 1.0F, subTreeSize / (subTreeSize + 8.0F));
-
-
-        // const calcColor = (tree: Node): number[] => {
-        //     let red: number = (primaryColor.r + secondaryColor.r) / 2 * (tree.depth / tree.maxDepth);
-        //     let green: number = (primaryColor.g + secondaryColor.g) / 2 * (tree.depth / tree.maxDepth);
-        //     let blue: number = (primaryColor.b + secondaryColor.b) / 2 * (tree.depth / tree.maxDepth);
-        //     let alpha: number = (primaryColor.a + secondaryColor.a) / 2 * (tree.depth / tree.maxDepth);
-        //     return [red,green,blue,alpha];
-        // }
+        const rectangle = [initialRectangleCenterX, initialRectangleCenterY, initialRectangleWidth, initialRectangleHeight, initialRectangleAngle];
 
         /** calculateCenter calculates the nX and Y coordinates of the new rectangle, relative to the X and Y of the old
          * rectangle. The values calculated still need to be offset by center X and Y coordinates of the old rectangle
@@ -55,24 +36,25 @@ export class GeneralizedPythagorasTree implements Visualizer {
          * @param {number} heightPrevious The height of the old rectangle
          * @param {number} width The width of the new rectangle
          * @param {number} height The Height of the new rectangle
-         * @param {number[]} newAngle The array containing the angles for the new rectangle
-         * @param {number} oldAngle The angle of rotation of the previous rectangle
+         * @param (number) prevAngle The angle of rotation of the previous child
+         * @param {number} newAngle The angle of rotation of the current child
+         * @param {number} angleParent The angle of rotation of the parent rectangle
          * @returns {number[]} Center X and Y coordinates of the new rectangle relative to the center X and Y coordinates of the old rectangle
          */
-        const calculateCenter = (widthPrevious: number, heightPrevious: number, width: number, height: number, newAngle: number[], oldAngle: number): number[] => {
+        const calculateCenter = (widthPrevious: number, heightPrevious: number, width: number, height: number, prevAngle: number, newAngle: number, angleParent: number): number[] => {
             // The center of the circle is located at the top middle of the parent rectangle
-            let centerCircle = [(widthPrevious / 2) * Math.cos(oldAngle + Math.PI / 2), (heightPrevious / 2) * Math.sin(oldAngle + Math.PI / 2)];
+            let centerCircle = [(heightPrevious / 2) * Math.cos(angleParent + Math.PI / 2), (heightPrevious / 2) * Math.sin(angleParent + Math.PI / 2)];
             // The coordinates of the new rectangle, bottom right refers to the first corner of the rectangle on the circle going counter clockwise
-            let bottomRight = [(widthPrevious / 2) * Math.cos(newAngle[newAngle.length - 2] + oldAngle), (heightPrevious / 2) * Math.sin(newAngle[newAngle.length - 2] + oldAngle)];
-            let bottomLeft = [(widthPrevious / 2) * Math.cos(newAngle[newAngle.length - 1] + oldAngle), (heightPrevious / 2) * Math.sin(newAngle[newAngle.length - 1] + oldAngle)];
+            let bottomRight = [(widthPrevious / 2) * Math.cos(prevAngle + angleParent), (widthPrevious / 2) * Math.sin(prevAngle + angleParent)];
+            let bottomLeft = [(widthPrevious / 2) * Math.cos(newAngle + angleParent), (widthPrevious / 2) * Math.sin(newAngle + angleParent)];
             // The mid point between the two corners calculated previously, relative to the previous rectangle
             let middle = [(bottomLeft[0] + bottomRight[0]) / 2, (bottomLeft[1] + bottomRight[1]) / 2];
 
             // This is the angle between the two lines going to the corners of the new rectangle
-            let angle = (newAngle[newAngle.length - 1] + newAngle[newAngle.length - 2]) / 2;
+            let angleDifference = (newAngle + prevAngle) / 2;
             // The center coordinates of the new rectangle
-            let centerX = (width / 2) * Math.cos(angle + oldAngle) + middle[0] + centerCircle[0];
-            let centerY = (height / 2) * Math.sin(angle + oldAngle) + middle[1] + centerCircle[1];
+            let centerX = (height / 2) * Math.cos(angleDifference + angleParent) + middle[0] + centerCircle[0];
+            let centerY = (height / 2) * Math.sin(angleDifference + angleParent) + middle[1] + centerCircle[1];
             return [centerX, centerY];
         };
 
@@ -104,14 +86,28 @@ export class GeneralizedPythagorasTree implements Visualizer {
              * recurse on the children of the element we are considering.
              */
             if (tree.subTreeSize > 1) { // If a node does not have children it does not have to draw its children
-                let angle: Array<number> = [0]; // Initialize to zero helps with calculating using the first angle
-
+                // Calculate the size of all the children of a parent ahead of time so that this only has to be done
+                // once for all the children of a node, instead of once per child of a node.
+                let sizeParent = 0; // Unfortunately have to always define otherwise typescript complains
+                if (drawNodeSize) {
+                    for (let child of tree.children) {
+                        sizeParent += child.length;
+                    }
+                }
+                let prevAngle;
+                let newAngle = 0;
                 // Loop over all the children of the node and recurse on each child
-                for (let element of tree.children) {
-                    angle.push(angle[angle.length - 1] + Math.PI * (element.subTreeSize / (element.parent.subTreeSize - 1))); // Calculate the angle of every subtree
-                    let width = rectangle[2] * Math.sin((angle[angle.length - 1] - angle[angle.length - 2]) / 2); // The last value in the array is always the one calculated last
-                    let height = weightHeight * rectangle[3] * Math.sin((angle[angle.length - 1] - angle[angle.length - 2]) / 2);
-                    let center = calculateCenter(rectangle[2], rectangle[3], width, height, angle, rectangle[4]);
+                for (let child of tree.children) {
+                    prevAngle = newAngle;
+                    if (!drawNodeSize) {
+                        newAngle = newAngle + Math.PI * (child.subTreeSize / (child.parent.subTreeSize - 1));
+                    } else {
+                        newAngle = newAngle + Math.PI * (child.length / sizeParent);
+                    }
+                    let sinAngleDifference = Math.sin((newAngle - prevAngle) / 2); // This saves us one sin calculation per child
+                    let width = rectangle[2] * sinAngleDifference; // The last value in the array is always the one calculated last
+                    let height = weightHeight * rectangle[3] * sinAngleDifference;
+                    let center = calculateCenter(rectangle[2], rectangle[3], width, height, prevAngle, newAngle, rectangle[4]);
 
                     // The previously calculated center coordinates still need to be offset by the old coordinates
                     center = [center[0] + rectangle[0], center[1] + rectangle[1]];
@@ -119,29 +115,24 @@ export class GeneralizedPythagorasTree implements Visualizer {
                     // The rotation of the new rectangle is equal to its angle relative to the angle of the rectangle that
                     // comes before it. It needs to be offset by the angle of the parent rectangle and then offset by
                     // -90 degrees for it to draw correctly using the fillRotatedQuad.
-                    let rotation = (angle[angle.length - 1] + angle[angle.length - 2]) / 2 + rectangle[4] - Math.PI / 2;
+                    let rotation = (newAngle + prevAngle) / 2 + rectangle[4] - Math.PI / 2;
                     let rectangleChild = [center[0], center[1], width, height, rotation];
                     // Finally we recurse using all the previously calculated values
-                    generate(element, rectangleChild, isSelected);
+                    generate(child, rectangleChild, isSelected);
                 }
             }
         };
 
         // Call the main recursive drawing function
         generate(tree, rectangle);
-
         return draws;
     }
 
     public getForm(formFactory: FormFactory): Form | null {
-        return null;
-        // return formFactory.createFormBuilder()
-        //     .addTextField('test1', 'TestValue', {label: 'Test label'})
-        //     .addNumberField('test2', 8, {label: 'Test label'})
-        //     .addToggleField('test3', false, {label: 'Test label'})
-        //     .addChoiceField('test4', 'test', {label: 'Test label', expanded: false, choices: {test: 'test'}})
-        //     .addChoiceField('test5', 'test', {label: 'Test label', expanded: true, choices: {test: 'test'}})
-        //     .getForm();
+        return formFactory.createFormBuilder()
+            .addSliderField("heightScale", 10, {label: "Height scale", min: 2, max: 20})
+            .addToggleField("nodeSize", false, {label: "Calculate using size of nodes",})
+            .getForm();
     }
 
     public getName(): string {
