9f2aaa9bb8a1c9c0ea069bf38ab79ac803bc1fe3
Roan <roan.hofland@hotmail.com>
2018-06-01 17:41:23 +0200

    New modes

--- src/opengl/shaders/shaderMode.ts
+++ src/opengl/shaders/shaderMode.ts
@@ -5,8 +5,14 @@ export enum ShaderMode{
 	FILL_CIRCLE = 1 << 2,
     DRAW_CIRCLE = 1 << 3,
     CIRCLES = LINED_CIRCLE | FILL_CIRCLE | DRAW_CIRCLE,
+    
+    //circle slices
+    LINED_CIRCLE_SLICE = 1 << 4,
+    FILL_CIRCLE_SLICE = 1 << 5,
+    DRAW_CIRCLE_SLICE = 1 << 6,
+    CIRCLE_SLICES = LINED_CIRCLE_SLICE | FILL_CIRCLE_SLICE | DRAW_CIRCLE_SLICE,
 
     //For debug usage only
-    ALL = CIRCLES,
+    ALL = CIRCLES | CIRCLE_SLICES,
 }
 /** @end-author Roan Hofland */  
