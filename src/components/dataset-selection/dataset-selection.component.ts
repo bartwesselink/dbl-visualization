1968958843f3139a2069c909f66f0e245764ba2c
Mathijs B <mathijs.boezer@gmail.com>
2018-06-03 10:41:06 +0200

    Add selectors in the menu

--- src/components/dataset-selection/dataset-selection.component.ts
+++ src/components/dataset-selection/dataset-selection.component.ts
@@ -1,11 +1,23 @@
 import {Component} from '@angular/core';
 
+interface DatasetFile {
+    title: string;
+    path: string;
+}
+
+
+
 @Component({
     selector: 'app-dataset-selection',
     templateUrl: './dataset-selection.component.html',
 })
 export class DatasetSelectionComponent {
     /** @author Mathijs Boezer */
+    public defaultDatasets: DatasetFile[] = [{title: 'NCBI Dataset', path: 'test1path'}, {title: 'Phylviz', path: 'test1path'}];
+    public userUploadedDatasets: DatasetFile[] = [{title: 'test1', path: 'test1path'}];
 
+    public loadDataset(path: string) {
+        console.log(path);
+    }
     /** @end-author Mathijs Boezer */
 }
