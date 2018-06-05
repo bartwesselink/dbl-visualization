0c7b89462ade91111373b9d45b65730c4ea59ea3
Nico <klaassen.nico@gmail.com>
2018-06-05 02:05:49 +0200

    Clean up and fix orientation recalculation issue

--- src/visualizations/simple-tree-map.ts
+++ src/visualizations/simple-tree-map.ts
@@ -26,7 +26,6 @@ export class SimpleTreeMap implements Visualizer {
         ];
         let totalNodes: number;
         let offset: number = settings.offset;
-        let offsetType: string = settings.offsetType;
         let tree: NodeTreeMap = originalTree as NodeTreeMap;
         let rootBounds: Bounds = {
             left: -(defaultSize / 2),
@@ -36,36 +35,8 @@ export class SimpleTreeMap implements Visualizer {
         };
         let treeHeight: number;
         let drawOutlines: boolean = settings.outline;
-        let offsetScale: number;
 
         drawOutlines = settings.outline;
-
-        // if (offsetType == 'fixed') {
-        //     // We have to create space to allow for the absolute offset of each cell
-        //     // Using this currently enables the best implementation for offset in all
-        //     // my extensive attempts thus far.
-        //     if (offset > 0) {
-        //         const oldBounds = rootBounds;
-        //         updateRootBounds();
-        //         const scaleFactor = (oldBounds.right / rootBounds.right); // Calculate a scalingfactor to prevent the visualization from moving
-        //         gl.scale(scaleFactor); // Actually scale
-        //         offsetScale = (300 / rootBounds.right); // Factor indicating how far away we are from normal scaling
-        //         offset = Math.pow(offset, 1.4); // Increase scaling factor for the offset, this can be tweaked, though 1.4 works well for now
-        //     }
-        // } else { // offsetType == 'relative'
-        //     const oldBounds = rootBounds;
-        //     // Reset root bounds when we select to apply our offset as 'relative'
-        //     rootBounds = {
-        //         left: -(defaultSize / 2),
-        //         right: (defaultSize / 2),
-        //         bottom: -(defaultSize / 2),
-        //         top: (defaultSize / 2),
-        //     };
-        //     const scaleFactor = (oldBounds.right / rootBounds.right); // Calculate a scalingfactor to prevent the visualization from moving
-        //     gl.scale(scaleFactor); // Actually scale
-        //     offsetScale = (300 / rootBounds.right); // Factor indicating how far away we are from normal scaling
-        //     offset = Math.pow(offset, 1.4); // Increase scaling factor for the offset, this can be tweaked, though 1.4 works well for now
-        // }
 
         // define used enums
         enum Orientation {
@@ -135,19 +106,6 @@ export class SimpleTreeMap implements Visualizer {
 
             // Compute the new bounds which are nested within the bounds of the parent
             if (tree.parent.orientation === Orientation.HORIZONTAL) {
-                // if (offsetType == 'fixed') {
-                //     const freeSpace = parentWidth - (tree.parent.children.length + 1) * offset;
-                //     return {
-                //         left: (index == 0) ?
-                //             parentBounds.left + offset :
-                //             parentBounds.left + offset * (index + 1) + (freeSpace * doneSize / parentSize),
-                //         right: (last) ?
-                //             parentBounds.left + offset * (index + 1) + freeSpace :
-                //             parentBounds.left + offset * (index + 1) + (freeSpace * (doneSize + childSize) / parentSize),
-                //         bottom: parentBounds.bottom + offset,
-                //         top: parentBounds.top - offset
-                //     };
-                // } else { // offsetType == 'relative'
                 const relativeOffset = Math.min(parentWidth / 100 * offset / (tree.parent.children.length + 1), parentHeight / 100 * offset / (tree.parent.children.length + 1));
                 const freeSpace = parentWidth - (tree.parent.children.length + 1) * relativeOffset;
                 return {
@@ -160,21 +118,7 @@ export class SimpleTreeMap implements Visualizer {
                     bottom: parentBounds.bottom + relativeOffset,
                     top: parentBounds.top - relativeOffset
                 };
-                // }
-            } else { // tree.parent.orientation === Orientation.VERTICAL
-                // if (offsetType == 'fixed') {
-                //     const freeSpace = parentHeight - (tree.parent.children.length + 1) * offset;
-                //     return {
-                //         left: parentBounds.left + offset,
-                //         right: parentBounds.right - offset,
-                //         bottom: (last) ?
-                //             parentBounds.top - offset * (index + 1) - freeSpace :
-                //             parentBounds.top - offset * (index + 1) - (freeSpace * (doneSize + childSize) / parentSize),
-                //         top: (index == 0) ?
-                //             parentBounds.top - offset :
-                //             parentBounds.top - offset * (index + 1) - (freeSpace * doneSize / parentSize)
-                //     };
-                // } else { // offsetType == 'relative'
+            } else {
                 const relativeOffset = Math.min(parentWidth / 100 * offset / (tree.parent.children.length + 1), parentHeight / 100 * offset / (tree.parent.children.length + 1));
                 const freeSpace = parentHeight - (tree.parent.children.length + 1) * relativeOffset;
                 return {
@@ -187,59 +131,9 @@ export class SimpleTreeMap implements Visualizer {
                         parentBounds.top - relativeOffset :
                         parentBounds.top - relativeOffset * (index + 1) - (freeSpace * doneSize / parentSize)
                 };
-                // }
             }
         };
 
-        // /**
-        //  * Recursive method to calculate the maximum width or height in terms of cell/segment count for a given tree.
-        //  *
-        //  * @param {Node} tree The tree for which to calculate the max segment width or height
-        //  * @param {number} horizontalSegments Initial call should be 0 , parameter to track width recursively
-        //  * @param {number} verticalSegments Initial call should be 0 , parameter to track height recursively
-        //  * @returns {number} The maximum width or height in terms of cell / segment count
-        //  */
-        // const calculateMaxSegments = (tree: NodeTreeMap, horizontalSegments: number, verticalSegments: number): number => {
-        //     let maxSegments = Math.max(horizontalSegments, verticalSegments);
-        //
-        //     for (let i = 0; i < tree.children.length; i++) {
-        //         if (maxSegments == 0) {
-        //             if (tree.orientation === Orientation.HORIZONTAL) {
-        //                 maxSegments = calculateMaxSegments(tree.children[i], horizontalSegments + tree.children.length, verticalSegments + 1);
-        //             } else {
-        //                 maxSegments = calculateMaxSegments(tree.children[i], horizontalSegments + 1, verticalSegments + tree.children.length);
-        //             }
-        //         } else {
-        //             let newMax;
-        //             if (tree.orientation === Orientation.HORIZONTAL) {
-        //                 newMax = calculateMaxSegments(tree.children[i], horizontalSegments + tree.children.length, verticalSegments + 1);
-        //             } else {
-        //                 newMax = calculateMaxSegments(tree.children[i], horizontalSegments + 1, verticalSegments + tree.children.length);
-        //             }
-        //
-        //             if (newMax > maxSegments) {
-        //                 maxSegments = newMax;
-        //             }
-        //
-        //         }
-        //     }
-        //     return maxSegments;
-        // }
-
-        // /**
-        //  * Utility function to calculate the new size for the upscaled base-square of the visualization
-        //  */
-        // const updateRootBound = (): void => {
-        //     // Caluclate how much offset we need at most based on width/height segment count.
-        //     const maxSegments = calculateMaxSegments(tree, 0, 0);
-        //
-        //     rootBounds = {
-        //         left: -(defaultSize / 2) - offset * treeHeight * maxSegments,
-        //         right: (defaultSize / 2) + offset * treeHeight * maxSegments,
-        //         bottom: -(defaultSize / 2) - offset * treeHeight * maxSegments,
-        //         top: (defaultSize / 2) + offset * treeHeight * maxSegments
-        //     };
-        // }
 
         /** drawTree draw the tree-map recursively.
          *
@@ -256,9 +150,22 @@ export class SimpleTreeMap implements Visualizer {
 
             // Draw the bounds of the current node
             if (drawOutlines) {
-                draws.push({ type: 6 /** FillLinedAAQuad **/, options: { x: bounds.left, y: bounds.bottom, width: width, height: height, fillColor: color, lineColor: [0, 0, 0, 1] } });
+                draws.push({
+                    type: 6 /** FillLinedAAQuad **/,
+                    options: {
+                        x: bounds.left,
+                        y: bounds.bottom,
+                        width: width,
+                        height: height,
+                        fillColor: color,
+                        lineColor: [0, 0, 0, 1]
+                    }
+                });
             } else {
-                draws.push({ type: 4 /** FillAAQuad **/, options: { x: bounds.left, y: bounds.bottom, width: width, height: height, color: color }});
+                draws.push({
+                    type: 4 /** FillAAQuad **/,
+                    options: {x: bounds.left, y: bounds.bottom, width: width, height: height, color: color}
+                });
             }
 
             // Compute color and size per child, recurse on each child with the new - and nested - bounds.
@@ -280,11 +187,12 @@ export class SimpleTreeMap implements Visualizer {
         };
 
         totalNodes = originalTree.subTreeSize;
-        treeHeight = calculateTreeHeight(tree, 0);
 
-        // Initialize orientation
-        tree.orientation = Orientation.HORIZONTAL;
-        orientTreeNodes(tree);
+        // Initialize orientation only when it's not yet defined
+        if (!tree.orientation) {
+            tree.orientation = Orientation.HORIZONTAL;
+            orientTreeNodes(tree);
+        }
 
         drawTree(tree, rootBounds, false, colorB);
 
@@ -295,7 +203,6 @@ export class SimpleTreeMap implements Visualizer {
         return formFactory.createFormBuilder()
             .addToggleField('outline', true, {label: 'Draw outlines'})
             .addSliderField('offset', 0, {label: 'Offset', min: 0, max: 25})
-            // .addChoiceField('offsetType', 'relative', { label: 'Offset type', expanded: false, choices: { relative: 'relative', fixed: 'fixed' } })
             .getForm();
     }
 
