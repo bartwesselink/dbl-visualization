5154e74bf9732c542adc8f855f6cce34759376d6
Mathijs B <mathijs.boezer@gmail.com>
2018-06-09 15:00:01 +0200

    Implement optimization features and change edges to quads

--- src/visualizations/basic-tree.ts
+++ src/visualizations/basic-tree.ts
@@ -4,10 +4,13 @@ import {FormFactory} from '../form/form-factory';
 import {Form} from "../form/form";
 import {Draw} from '../interfaces/draw';
 import {VisualizerInput} from '../interfaces/visualizer-input';
+import {ShaderMode} from "../opengl/shaders/shaderMode";
+import {OpenGL} from "../opengl/opengl";
 
 /** @author Mathijs Boezer */
 export class BasicTree implements Visualizer {
     public draw(input: VisualizerInput): Draw[] {
+        let t0 = +(new Date());
         const tree: Node = input.tree;
         const draws: Draw[] = [];
         const widthMultiplier = input.settings.width; // distance between nodes horizontally
@@ -15,6 +18,7 @@ export class BasicTree implements Visualizer {
         const color = [0, 0, 0, 1]; // black
         const selectedColor = [0.4, 0.4, 0.4, 1]; // grey
         const yOffset = -400; // move the origin to the bottom instead of middle with 50px of spacing
+        const radianToDegreeMultiplier = 180 / Math.PI;
 
         let node = tree;
         let height = 0;
@@ -33,7 +37,6 @@ export class BasicTree implements Visualizer {
 
         const generate: (tree: Node, origin: number[], selectedAncestor: boolean) => void = (tree: Node, origin: number[], selectedAncestor: boolean): void => {
             selectedAncestor = selectedAncestor || tree.selected; // whether this node or an ancestor is selected determines its color
-
             if (tree.children.length > 0) {
                 let x: number = -(tree.subTreeSize - 1.0) * widthMultiplier; // relative x coordinate for leftmost child
                 let y: number = tree.subTreeSize * heightMultiplier; // relative y coordinate for children
@@ -42,17 +45,26 @@ export class BasicTree implements Visualizer {
                     x += child.subTreeSize * widthMultiplier; // first add subtree size to x
                     childOrigin = [origin[0] + x, origin[1] + y]; // calculate origin for child
 
-                    // draw edge to child
+                    let middleX = (origin[0] + childOrigin[0]) / 2; // middle point of edge from parent to child
+                    let middleY = (origin[1] + childOrigin[1]) / 2; // middle point of edge from parent to child
+                    let height = ((origin[0] - childOrigin[0])**2 + (origin[1] - childOrigin[1])**2)**0.5; // edge length
+                    let angle = Math.acos((origin[1] - childOrigin[1]) / height); // angle between y axis and edge
+                    angle = angle * radianToDegreeMultiplier; // convert to degrees
+                    angle = childOrigin[0] < origin[0] ? -angle : angle; // negate if it should rotate counterclockwise
+
+                    // draw 'edge' to children, its actually a quad though
                     draws.push({
-                        type: 15, // line
+                        type: 1, // filled rotated quad
                         options: {
-                            x1: origin[0] * scalar,
-                            y1: origin[1] * scalar + yOffset,
-                            x2: childOrigin[0] * scalar,
-                            y2: childOrigin[1] * scalar + yOffset,
-                            color: color,
+                            x: middleX * scalar,
+                            y: middleY  * scalar + yOffset,
+                            width: child.subTreeSize * scalar, // subtree size as width
+                            height: height * scalar,
+                            rotation: angle,
+                            color: selectedAncestor ? selectedColor : color,
                         }
                     });
+
                     generate(child, childOrigin, selectedAncestor); // draw child node and subtree
                     x += child.subTreeSize * widthMultiplier; // add subtree size again so the next child won't overlap with the current
                 }
@@ -88,5 +100,21 @@ export class BasicTree implements Visualizer {
     public getThumbnailImage(): string|null {
         return '/assets/images/visualization-basic-tree.png';
     }
+
+    public enableShaders(gl: OpenGL): void {
+        console.log("test");
+        gl.enableShaders(ShaderMode.FILL_CIRCLE);
+    }
+
+    // public optimizeShaders(gl: OpenGL): void {
+    //     gl.optimizeDefault();
+    //     gl.optimizeFor(ShaderMode.FILL_CIRCLE);
+    //     gl.optimizeFor(ShaderMode.DRAW_CIRCLE);
+    //     gl.optimizeFor(ShaderMode.DRAW_CIRCLE_SLICE);
+    //     gl.optimizeFor(ShaderMode.FILL_CIRCLE_SLICE);
+    //     gl.optimizeFor(ShaderMode.DRAW_RING_SLICE);
+    //     gl.optimizeFor(ShaderMode.FILL_RING_SLICE);
+    //     gl.optimizeFor(ShaderMode.CIRCULAR_ARC);
+    // }
 }
 /** @end-author Mathijs Boezer */
