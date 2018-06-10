badd9997ebcc2a8ebe02243600027f661eff2aa0
Mathijs B <mathijs.boezer@gmail.com>
2018-06-10 10:16:48 +0200

    Grow down and node size option and swap colors

--- src/visualizations/basic-tree.ts
+++ src/visualizations/basic-tree.ts
@@ -10,13 +10,16 @@ import {OpenGL} from "../opengl/opengl";
 /** @author Mathijs Boezer */
 export class BasicTree implements Visualizer {
     public draw(input: VisualizerInput): Draw[] {
-        const tree: Node = input.tree;
-        const draws: Draw[] = [];
         const widthMultiplier = input.settings.width; // distance between nodes horizontally
         const heightMultiplier = input.settings.height; // the vertical distance between nodes in terms of subtree size
-        const color = [0, 0, 0]; // black
-        const selectedColor = [0.4, 0.4, 0.4]; // grey
-        const yOffset = -400; // move the origin to the bottom instead of middle with 50px of spacing
+        const nodeWidth = input.settings.nodeWidth / 100; // whether the nodes are the same width as the edges, or bigger divide by 100 since we only use integers in sliders
+        const growDown = input.settings.growDown; // whether the root of the tree is at the top and grows upwards, or the inverse
+
+        const tree: Node = input.tree;
+        const draws: Draw[] = [];
+        const selectedColor = [0, 0, 0]; // black
+        const color = [0.4, 0.4, 0.4]; // grey
+        const yOffset = -400 * (growDown ? -1 : 1); // move the origin to the bottom (or top if grow down) instead of middle with 50px of spacing
         const radianToDegreeMultiplier = 180 / Math.PI;
 
         let node = tree;
@@ -32,7 +35,9 @@ export class BasicTree implements Visualizer {
             }
             node = maxChild;
         }
-        const scalar: number = (900 / height) * 0.9; // number to multiply each coordinate by to fit the tree by height in the window with 10% spacing around it
+        const scalarX: number = (900 / height) * 0.9; // number to multiply each coordinate by to fit the tree by height in the window with 10% spacing around it
+        const scalarY = scalarX * (growDown ? -1 : 1);
+        console.log(scalarX, scalarY);
 
         const generate: (tree: Node, origin: number[], selectedAncestor: boolean) => void = (tree: Node, origin: number[], selectedAncestor: boolean): void => {
             selectedAncestor = selectedAncestor || tree.selected; // whether this node or an ancestor is selected determines its color
@@ -50,15 +55,15 @@ export class BasicTree implements Visualizer {
                     let angle = Math.acos((origin[1] - childOrigin[1]) / height); // angle between y axis and edge
                     angle = angle * radianToDegreeMultiplier; // convert to degrees
                     angle = childOrigin[0] < origin[0] ? -angle : angle; // negate if it should rotate counterclockwise
-
+                    angle = growDown ? -angle : angle; // negate if growing downwards
                     // draw 'edge' to children, its actually a quad though
                     draws.push({
                         type: 1, // filled rotated quad
                         options: {
-                            x: middleX * scalar,
-                            y: middleY  * scalar + yOffset,
-                            width: child.subTreeSize * scalar, // subtree size as width
-                            height: height * scalar,
+                            x: middleX * scalarX,
+                            y: middleY  * scalarY + yOffset,
+                            width: child.subTreeSize * scalarX, // subtree size as width
+                            height: height * scalarX,
                             rotation: angle,
                             color: selectedAncestor ? selectedColor : color,
                         }
@@ -73,9 +78,9 @@ export class BasicTree implements Visualizer {
             draws.push({
                 type: 10, // filled circle
                 options: {
-                    x: origin[0] * scalar,
-                    y: origin[1] * scalar + yOffset,
-                    radius: tree.subTreeSize * scalar * 0.5, // subtree size as diameter
+                    x: origin[0] * scalarX,
+                    y: origin[1] * scalarY + yOffset,
+                    radius: tree.subTreeSize * scalarX * nodeWidth, // subtree size as diameter or radius depending on the settings
                     color: selectedAncestor ? selectedColor : color,
                 },
                 identifier: tree.identifier, // selecting on the nodes
@@ -89,6 +94,8 @@ export class BasicTree implements Visualizer {
         return formFactory.createFormBuilder()
             .addSliderField('width', 5, { label: 'Width', min: 0.5, max: 10 })
             .addSliderField('height', 3, { label: 'Height', min: 0.5, max: 10 })
+            .addSliderField('nodeWidth', 75, { label: 'Node size', min: 50, max: 100 }) // sliders only work on integers afaik
+            .addToggleField('growDown', false, {label: 'Grown downwards'})
             .getForm();
     }
 
