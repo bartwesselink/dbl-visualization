c3358ea0ee4656e2ef4b5f1f99c2e075536fd067
Nico <klaassen.nico@gmail.com>
2018-06-12 23:18:45 +0200

    added snackbar support

--- src/utils/newick-exporter.ts
+++ src/utils/newick-exporter.ts
@@ -3,27 +3,32 @@ import * as FileSaver from "file-saver";
 
 /** @author Nico Klaassen */
 export class NewickExporter {
-    errorMsg = "Invalid Newick file.";
-    successMsg = "Succefully parsed Newick file.";
+    errorMsg = "Error generating Newick data from selected tree.";
+    successMsg = "Niwick data ready for download.";
 
     constructor(private snackbar: any) {
     }
 
     public exportTree(tree: Node): void {
-        const newickString = this.parseTree(tree) + ";";
-        const now = new Date();
-        const month = now.getMonth() > 9 ? now.getMonth() : (0).toString() + now.getMonth();
-        const day = now.getDate() > 9 ? now.getDate() : (0).toString() + now.getDate();
-        const hours = now.getHours() > 9 ? now.getHours() : (0).toString() + now.getHours();
-        const minutes = now.getMinutes() > 9 ? now.getMinutes() : (0).toString() + now.getMinutes();
-        const fileName = "export_" + now.getFullYear() + "-" +
-                                     month + "-" +
-                                     day + "_" +
-                                     hours +
-                                     minutes + ".ngl";
+        try {
+            const newickString = this.parseTree(tree) + ";";
+            const now = new Date();
+            const month = now.getMonth() > 9 ? now.getMonth() : (0).toString() + now.getMonth();
+            const day = now.getDate() > 9 ? now.getDate() : (0).toString() + now.getDate();
+            const hours = now.getHours() > 9 ? now.getHours() : (0).toString() + now.getHours();
+            const minutes = now.getMinutes() > 9 ? now.getMinutes() : (0).toString() + now.getMinutes();
+            const fileName = "export_" + now.getFullYear() + "-" +
+                month + "-" +
+                day + "_" +
+                hours +
+                minutes + ".ngl";
 
-        const file = new File([newickString], fileName, {type: "text/plain"});
-        FileSaver.saveAs(file);
+            const file = new File([newickString], fileName, {type: "text/plain"});
+            this.feedback(this.successMsg);
+            FileSaver.saveAs(file);
+        } catch {
+            this.feedback(this.errorMsg);
+        }
     }
 
 
@@ -41,7 +46,7 @@ export class NewickExporter {
             newickString = newickString.substring(0, newickString.length - 1) + ")";
         }
 
-        newickString += tree.label; //+ ":" + tree.length;
+        newickString += tree.label + ":" + tree.length;
         return newickString;
     }
 
