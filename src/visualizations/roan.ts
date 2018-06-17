695a30a330d8d33c36544195d00ebad017e0f3ef
Roan <roan.hofland@hotmail.com>
2018-06-17 18:32:15 +0200

    Comply with visualisation interface changes

--- src/visualizations/roan.ts
+++ src/visualizations/roan.ts
@@ -9,27 +9,20 @@ import {Node} from "../models/node";
 import {Palette} from "../models/palette";
 
 export class Roan implements Visualizer {
+    public shapesPerNode: number = 1;
     
     public draw(input: VisualizerInput): Draw[] {
             const tree = input.tree;
             const draws = new Array();
-            const palette: Palette = input.palette;
-            var color: number[];
             
             const compute = (parent: Node, cx: number, cy: number, rad: number): void => {
-                if (parent.selected === true) {
-                    color = palette.gradientColorMapSelected[parent.maxDepth][parent.depth];
-                } else {
-                    color = palette.gradientColorMap[parent.maxDepth][parent.depth];
-                }
                 draws.push({ 
                     type: 10 /** FillCircle **/, 
                     identifier: parent.identifier,
                     options: { 
                         x: cx, 
                         y: cy, 
-                        radius: rad, 
-                        color: color 
+                        radius: rad
                     } 
                 });
                 if(parent.subTreeSize > 0){
@@ -63,6 +56,22 @@ export class Roan implements Visualizer {
 
     public enableShaders(gl: OpenGL): void {
         gl.enableShaders(ShaderMode.FILL_CIRCLE);
+    }
+    
+    public updateColors(gl: OpenGL, input: VisualizerInput, draws: Draw[]): void{
+        this.recolor(input.tree, input.palette, gl, draws, input.tree.selected);
+    }
+    
+    private recolor(tree: Node, palette: Palette, gl: OpenGL, draws: Draw[], selected: boolean){
+        if (selected || tree.selected) {
+            selected = true;
+            gl.setColor(draws[tree.identifier].glid, palette.gradientColorMapSelected[tree.maxDepth][tree.depth]);
+        } else {
+            gl.setColor(draws[tree.identifier].glid, palette.gradientColorMap[tree.maxDepth][tree.depth]);
+        }
+        for(let child of tree.children){
+            this.recolor(child, palette, gl, draws, selected);
+        }
     }
 }
 /** @end-author Roan Hofland */
