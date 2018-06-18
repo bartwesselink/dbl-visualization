3a605f9f3a009848997db6bd0370de56f43a13c8
Nico <klaassen.nico@gmail.com>
2018-06-18 21:30:25 +0200

    Cleanup of unused variables and slight change to internodal offset

--- src/visualizations/space-reclaiming-stack.ts
+++ src/visualizations/space-reclaiming-stack.ts
@@ -21,52 +21,12 @@ export class SpaceReclaimingStack implements Visualizer {
         let height = settings.height;
         let width = settings.width;
         let reclaimCoefficient = settings.reclaimCoefficient;
-
         const levelHeight = height / originalTree.maxDepth;
-
-
-        const defaultSize = 600;
-        let colorA: number[] = [255 / 255, 153 / 255, 0, 1];
-        let colorB: number[] = [51 / 255, 0, 255 / 255, 1];
-        let defaultLineColor: number[] = [0, 0, 0, 1];
-        let lineColor: number[] = defaultLineColor;
-        let selectedColor: number[] = [255 / 255, 100 / 255, 0, 1];
-        let colorDifference: number[] = [
-            colorB[0] - colorA[0],
-            colorB[1] - colorA[1],
-            colorB[2] - colorA[2],
-            colorB[3] - colorA[3]
-        ];
-        let treeHeight: number;
-        let drawOutlines: boolean = settings.outline;
 
         let sortedNodes: any;
         let startPoints: any;
         let endPoints: any;
 
-        drawOutlines = settings.outline;
-
-        /**
-         * Function which calculates the height of the given tree recursively
-         *
-         * @param {Node} tree Tree for which to calculate the height for
-         * @param {number} currentHeight Initially should be 0, variable to track current height.
-         * @returns {number} The height of the tree
-         */
-        const calculateTreeHeight = (tree: NodeSpaceReclaimingStack, currentHeight: number): number => {
-            let treeHeight = currentHeight;
-            for (let i = 0; i < tree.children.length; i++) {
-                if (treeHeight == 0) {
-                    treeHeight = calculateTreeHeight(tree.children[i], currentHeight + 1);
-                } else {
-                    const newHeight = calculateTreeHeight(tree.children[i], currentHeight + 1);
-                    if (newHeight > treeHeight) {
-                        treeHeight = newHeight;
-                    }
-                }
-            }
-            return treeHeight;
-        };
 
         /** In-order tree walk to store the nodes in an array based on depth
          *
@@ -90,32 +50,17 @@ export class SpaceReclaimingStack implements Visualizer {
 
         const simpleCompute = (): void => {//tree:NodeSpaceReclaimingStack, index: number): void => {
             for (let depth = 0; depth < sortedNodes.length; depth++) {
-                // console.log("=-=-=-=-=-=-=");
-                console.log(depth);
-                const offset = 6; //width / (tree.parent.children.length - 1 ) * 2 * 0.025; // 0.1 = 10%
+                const offset = Math.min(width / sortedNodes[depth].length * 0.025, 4);
                 const segmentWidth = (width - offset * (sortedNodes[depth].length - 1)) / sortedNodes[depth].length;
-                // console.log("segwidth");
-                // console.log(segmentWidth);
-                let oldParent = sortedNodes[depth][0].parent;
-                let parent = null;
-                let differentParent = false;
                 let left = - width / 2;
                 let right = left + segmentWidth;
+
                 for (let i = 0; i < sortedNodes[depth].length; i++) {
-                    // console.log(left);
                     const tree = sortedNodes[depth][i];
                     const topY = height / 2 - levelHeight * tree.depth;
                     const bottomY = height / 2 - levelHeight * (tree.depth + 1);
-                    // console.log(tree.parent);
+
                     if (tree.parent) {
-                        parent = tree.parent;
-                        // if (parent === oldParent || parent === null) {
-                        //     differentParent = false;
-                        // } else {
-                        //     differentParent = true;
-                            // left += offset;
-                            // right += offset;
-                        // }
                         if (tree.parent.children.length > 1) {
                             const width = Math.abs(tree.parent.bottomleft[0] - tree.parent.bottomright[0]);
                             const index = calculateIndex(tree);
@@ -150,38 +95,14 @@ export class SpaceReclaimingStack implements Visualizer {
                         tree.bottomleft = [- width / 2, bottomY];
                         tree.bottomright = [width / 2, bottomY];
                     }
+
                     left += segmentWidth + offset;
                     right += segmentWidth + offset;
-                    oldParent = parent;
                 }
             }
         };
 
-        const scaleFullWidth = (depth: number): void => {
-            let leftX = sortedNodes[depth][0].bottomleft[0];
-            let rightX = sortedNodes[depth][sortedNodes[depth].length - 1].bottomright[0];
-            let width = Math.abs(leftX) + Math.abs(rightX);
-            for (let i = 0; i < sortedNodes[depth].length; i++) {
-                const node = sortedNodes[depth][i];
-                node.bottomleft[0] = ((node.bottomleft[0] + Math.abs(leftX)) / width) * 600 - 300;
-                node.bottomright[0] = ((node.bottomright[0] + Math.abs(leftX)) / width) * 600 - 300;
-            }
-
-        };
-
         const recursiveDraw = (tree: NodeSpaceReclaimingStack, selected: boolean = false): void => {
-            // if (tree.selected) {
-            //     selected = true;
-            //
-            //     if (!drawOutlines) {
-            //         color = selectedColor;
-            //     } else {
-            //         lineColor = selectedColor;
-            //     }
-            // } else {
-            //     lineColor = defaultLineColor;
-            // }
-
             // Draw the bounds of the current node
             draws.push({ type: 23 /** FillCustomQuad **/,
                          identifier: tree.identifier,
@@ -197,25 +118,18 @@ export class SpaceReclaimingStack implements Visualizer {
         };
 
         // Initialize the arrays to sort and draw the nodes by depth
-        treeHeight = originalTree.maxDepth;
         sortedNodes = [];
         startPoints = [];
         endPoints = [];
-        for (let i = 0; i <= treeHeight; i++) {
+        for (let i = 0; i <= originalTree.maxDepth; i++) {
             sortedNodes.push([]);
             startPoints.push([]);
             endPoints.push([]);
         }
 
-        console.log("simpleCompute start");
-        recursiveDepthSort(originalTree);
-        console.log("recursiveDepthSort start");
-        simpleCompute();//originalTree, 0);
-        console.log("scaleFullWidth start");
-        // scaleFullWidth();
-        console.log("recursiveDraw start");
-        recursiveDraw(originalTree, false);
-        console.log("done");
+        recursiveDepthSort(originalTree); // Sort nodes by level in a nested array
+        simpleCompute(); // Compute all the coordinates for all of the nodes
+        recursiveDraw(originalTree, false); // Compute all draws for the tree
 
         return draws;
     }
@@ -225,7 +139,6 @@ export class SpaceReclaimingStack implements Visualizer {
             .addNumberField('height', 800, {label: 'Height'})
             .addNumberField('width', 600, {label: 'Width'})
             .addSliderField('reclaimCoefficient', 50, {label: "Reclaiming coefficient", min: 0, max: 100})
-            // .addChoiceField('offsetType', 'relative', { label: 'Offset type', expanded: false, choices: { relative: 'relative', fixed: 'fixed' } })
             .getForm();
     }
 
