c9a0829528280875056dd03fc4d96f187a86a6fc
teards <teards@nooby.eu>
2018-06-18 14:17:02 +0200

    Extra palettes, cleaned up palette names, NGL branding 8)

--- src/utils/palettes.ts
+++ src/utils/palettes.ts
@@ -2,7 +2,7 @@ import {Palette} from '../models/palette'
 import {Color} from './color'
 
 export class Palettes {
-    static default = new Palette(
+    static defaultBlue = new Palette(
         Color.fromHex('112C61'),
         Color.fromHex('2C95FF'),
         [
@@ -11,7 +11,7 @@ export class Palettes {
             Color.fromHex('EDEDED')
         ]
     );
-    static alt = new Palette(
+    static redBlue = new Palette(
         Color.fromHex('F21D2F'),
         Color.fromHex('023859'),
         [
@@ -27,6 +27,51 @@ export class Palettes {
             Color.fromHex('595959'),
             Color.fromHex('A6A6A6'),
             Color.fromHex('D9D9D9')
+        ]
+    );
+    static vaporWave = new Palette(
+        Color.fromHex('cbffe6'),
+        Color.fromHex('afe9ff'),
+        [
+            Color.fromHex('bfb9ff'),
+            Color.fromHex('ffcfea'),
+            Color.fromHex('feffbe')
+        ]
+    );
+    static malachite = new Palette(
+        Color.fromHex('5fbd98'),
+        Color.fromHex('193e2c'),
+        [
+            Color.fromHex('3b9b75'),
+            Color.fromHex('2c8863'),
+            Color.fromHex('1e583f')
+        ]
+    );
+    static candy = new Palette(
+        Color.fromHex('eef970'),
+        Color.fromHex('c900bd'),
+        [
+            Color.fromHex('ff0000'),
+            Color.fromHex('023bff'),
+            Color.fromHex('c2fffd')
+        ]
+    );
+    static goldenBlue = new Palette(
+        Color.fromHex('3773ff'),
+        Color.fromHex('f4cc24'),
+        [
+            Color.fromHex('f29200'),
+            Color.fromHex('002472'),
+            Color.fromHex('0037ab')
+        ]
+    );
+    static neon = new Palette(
+        Color.fromHex('00ff6b'),
+        Color.fromHex('ff00db'),
+        [
+            Color.fromHex('00dfff'),
+            Color.fromHex('fdff00'),
+            Color.fromHex('7c00ff')
         ]
     );
 }
