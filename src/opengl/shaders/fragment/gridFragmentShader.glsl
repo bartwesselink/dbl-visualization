d73943963ea08b3bc70fc10aad88bad855078681
Roan <roan.hofland@hotmail.com>
2018-06-26 16:59:02 +0200

    Multiplication should be more efficient than variable allocation

--- src/opengl/shaders/fragment/gridFragmentShader.glsl
+++ src/opengl/shaders/fragment/gridFragmentShader.glsl
@@ -10,7 +10,6 @@ uniform lowp float rx;
 uniform lowp float ry;
 
 void main() {
-	lowp float x = vpos.x * ratio;
-	gl_FragColor = vec4(color, color, color, 0.15 - 100.0 * min(abs(mod(x * rx + vpos.y * ry - dx * ratio, 0.1) - 0.05), abs(mod(x * ry - vpos.y * rx + dy, 0.1) - 0.05)));
+	gl_FragColor = vec4(color, color, color, 0.15 - 100.0 * min(abs(mod(vpos.x * ratio * rx + vpos.y * ry - dx * ratio, 0.1) - 0.05), abs(mod(vpos.x * ratio * ry - vpos.y * rx + dy, 0.1) - 0.05)));
 }
 /** @end-author Roan Hofland */
